da4cbd62ba5c6b03e7e26a2bb0e86385b6498511
Erik Mansén <erima668@student.liu.se>
2016-05-13 23:11:55 +0200

    PlayerScripts folder update
    Added folders for player scripts

---
+++ Left for Schlager/Assets/Scripts/PlayerScripts/PlayerStatusScript.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerStatusScript : MonoBehaviour {
+
+    private HealthComponent health;
+    private int rationCount;
+
+    // Use this for initialization
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    //OnTriggerEnter
+}
