0f3fe58e1931f5ddd80c697702e96802d404508e
Erik Mansén <erima668@student.liu.se>
2016-05-14 16:21:35 +0200

    MacheteScript allows control of machete animation and collision detection

---
+++ Left for Schlager/Assets/Scripts/WeaponScripts/MacheteScript.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MacheteScript : MonoBehaviour{
+
+    [SerializeField]
+    private bool attacking = false;
+    private float attackDamage;
+    private Animator animator;
+
+    // Use this for initialization
+    void Start() {
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void SwingMachete() {
+        if (attacking == false)
+        {
+            attacking = true;
+            animator.SetBool("attacking", true);
+        }
+    }
+
+    public void StopAttacking()
+    {
+        attacking = false;
+        animator.SetBool("attacking", false);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Zombie") && attacking)
+        {
+            //Deal damage to zombie
+        }
+    }
+}
